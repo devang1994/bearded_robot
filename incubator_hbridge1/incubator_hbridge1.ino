ecee9c5cd1bdcc98219952b05c2bc02b3e2b9dd2
devang <devang1994@gmail.com>
2014-08-06 14:10:52 +0000

    cleaned some junk, sorted hbridge turning off

--- incubator_hbridge1/incubator_hbridge1.ino
+++ incubator_hbridge1/incubator_hbridge1.ino
@@ -1,58 +1,54 @@
 #include "DHT.h"
 #define DHTTYPE DHT22
-#define DHTPIN 13
+#define DHTPIN 2
 DHT dht(DHTPIN, DHTTYPE);
-int hPin=13;
-int cPin=12;
-
-//in current setup 1 cools box, 2 heats box, 0 shuts it down
-
+#define hPin 13
+#define cPin 12
 void setup()
 {
   Serial.begin(9600);
   pinMode(hPin,OUTPUT);
   pinMode(cPin,OUTPUT);
   dht.begin();
+  Serial.println("1 to cool, 2 to heat and 0 to do nothing");
 }
 
 void loop()
 {
-  while(Serial.available()>0)
+  int b1=Serial.parseInt();
+  if (Serial.read() == '\n') 
   {
-    
-    
-    int b1=Serial.parseInt();
-    if (Serial.read() == '\n') 
+    if(b1==1)
     {
-      if(b1==1)
-      {
-        digitalWrite(hPin, HIGH);
-        digitalWrite(cPin, LOW);
-      }
-      if(b1==2)
-      {
-        digitalWrite(hPin, LOW);
-        digitalWrite(cPin, HIGH);
-
-      }
-      if(b1==0)
-      {
-        digitalWrite(hPin, LOW);
-        digitalWrite(cPin, LOW);
-      }
+      Serial.println("Cooling");
+      digitalWrite(hPin, HIGH);
+      digitalWrite(cPin, LOW);
     }
-    
-    float t = dht.readTemperature();
-    if (isnan(t)) 
+    if(b1==2)
     {
-      Serial.println("Failed to read from DHT sensor!");
+	Serial.println("HEAT");
+      digitalWrite(hPin, LOW);
+      digitalWrite(cPin, HIGH);
     }
-    
-    Serial.print("Temperature: "); 
-    Serial.print(t);
-    Serial.println(" *C "); 
-    delay(3000);
+    if(b1==0)
+    {
+      Serial.println("idleee");
+      digitalWrite(hPin, LOW);
+      digitalWrite(cPin,LOW);
+    }
   }
+
+  float t = dht.readTemperature();
+  if (isnan(t)) 
+  {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C "); 
+  delay(3000);
+
 }
-      
+
 
